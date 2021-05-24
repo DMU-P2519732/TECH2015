e0856b5e7941cb2df642ea673154c0c23900c90a
DMU-P2519732 <72402457+DMU-P2519732@users.noreply.github.com>
2021-05-24 02:10:59 +0100

    Add files via upload

--- TECH2015_CW2_HATHAWAY_Freddy/js/script.js
+++ TECH2015_CW2_HATHAWAY_Freddy/js/script.js
@@ -1,8 +1,3 @@
-const lightbox = GLightbox({
-  touchNavigation: true,
-  loop: true,
-  autoplayVideos: true
-});
 
 //Get the button:
 mybutton = document.getElementById("myBtn");
@@ -22,4 +17,4 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+};
