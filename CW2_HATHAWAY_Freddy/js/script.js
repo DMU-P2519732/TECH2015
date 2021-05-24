b1467a7cadfe6cfabc4492651a9ffdd819ac1916
DMU-P2519732 <72402457+DMU-P2519732@users.noreply.github.com>
2021-05-24 20:50:19 +0100

    Add files via upload

--- CW2_HATHAWAY_Freddy/js/script.js
+++ CW2_HATHAWAY_Freddy/js/script.js
@@ -2,7 +2,7 @@
 //Get the button:
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// Fuction is created so when the user scrolls 20px down from the top of the document, the button shows
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -13,8 +13,8 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// Fuction is created to when the user clicks on the button, it scrolls to the top of the page
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; // This is for Safari
+  document.documentElement.scrollTop = 0; // This is for Chrome, Firefox, IE and Opera
 };
